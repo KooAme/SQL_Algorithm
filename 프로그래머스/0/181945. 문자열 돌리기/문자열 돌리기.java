c4f64d12d1ecf71c989fb55021c04ec2886370cf
Koo <dg.changeworld@gmail.com>
2025-04-02 22:59:47 +0900

    [level 0] Title: 문자열 돌리기, Time: 201.09 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -6,8 +6,8 @@ public class Solution {
         String a = sc.next();
         String[] arr = a.split("");
         
-        for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
+        for (String s : arr) {
+            System.out.println(s);
         }
         
     }
